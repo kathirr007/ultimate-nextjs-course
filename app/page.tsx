a10c39053a2ca1a2ccc14f2644f76d5ee79d0b2c
Kathirr007 <kathirr007@gmail.com>
2025-12-23 05:21:59 +0530

    updated assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 function Home() {
   return (
-    <h1 className="text-4xl font-bold text-center">Welcome to Ultimate Next.js 15 Course</h1>
+    <>
+      <h1 className="h1-bold">Welcome to Ultimate Next.js 15 Course</h1>
+      <h1 className="h1-bold font-inter">Welcome to Ultimate Next.js 15 Course</h1>
+    </>
   )
 }
 
