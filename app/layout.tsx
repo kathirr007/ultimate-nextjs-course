a3988123f269778de8734a32006a1780ba2d47bc
Kathirr007 <kathirr007@gmail.com>
2025-12-23 15:20:32 +0530

    Added next-themes for dark/light themes toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
+import ThemeProvider from '@/context/Theme'
 import './globals.css'
 
 const inter = localFont({
@@ -29,11 +30,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
